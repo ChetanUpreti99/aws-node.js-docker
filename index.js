3c2b7252fe2a79c9b45e8a3b30430c58472cf5d6
Chetan Upreti <chetanupreti@Chetans-MacBook-Air-2.local>
2022-05-10 11:26:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/test', (req, res) => {
+  res.send('Successful response.');
+});
+
+app.listen(6000, () => console.log('Example app is listening on port 6000.'));
